b4c0c7018606171006cf33171c228616db312267
Karan Amal  Pradhan <kpradhan@Karans-MacBook-Pro.local>
2025-09-01 09:03:30 -0700

    Add Double-Checked Locking Singleton implementation

--- singleton/Singleton.java
+++ singleton/Singleton.java
@@ -10,7 +10,9 @@ In Java, one of the ways to implement Singleton is by making the constructor pri
 // If an instance already exists, it skips the creation step.
 
 /*Warning:
-This implementation is not thread-safe. If multiple threads call getInstance() simultaneously when instance is null, it's possible to create multiple instances.*/
+This implementation is not thread-safe. If multiple threads call getInstance() simultaneously when instance is null, it's possible to create multiple instances.
+We can make it Thread safe, by making the method synchronized, so that other thread must wait until method gets executed, but doing so can cause substantial overhead
+and reduce performance, which can bottleneck if called frequently.*/
 class LazySingleton {
     private static LazySingleton instance;
 
@@ -22,3 +24,28 @@ class LazySingleton {
         return instance;
     }
 };
+
+/* 3. Double-Checked Locking
+This approach minimizes performance overhead from synchronization by only synchronizing when the object is first created.
+
+It uses the volatile keyword to ensure that changes to the instance variable are immediately visible to other threads. */
+// If the first check (instance == null) passes, we synchronize on the class object.
+// We check the same condition one more time because multiple threads may have passed the first check.
+// The instance is created only if both checks pass.
+class DoubleCheckingSingleton {
+    private static volatile DoubleCheckingSingleton instance;
+
+    private DoubleCheckingSingleton(){}
+
+    public static DoubleCheckingSingleton getInstance(){
+        if(instance == null){
+            synchronized (DoubleCheckingSingleton.class){
+                if(instance == null){
+                    instance = new DoubleCheckingSingleton();
+                }
+            }
+        }
+        
+        return instance;
+    }
+}
