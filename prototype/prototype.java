feebfff8de4b0d6435b74202c06ac83dbbb62c22
Karan Amal  Pradhan <kpradhan@Karans-MacBook-Pro.local>
2025-09-04 08:57:13 -0700

    Add Prototype pattern example

---
+++ prototype/prototype.java
@@ -0,0 +1,43 @@
+interface EnemyPrototype { 
+    EnemyPrototype clone();
+}
+
+class Enemy implements EnemyPrototype {
+    private String type;
+    private int health;
+    private double speed;
+    private boolean armored;
+    private String weapon;
+
+    public Enemy(String type, int health, double speed, boolean armored, String weapon) {
+        this.type = type;
+        this.health = health;
+        this.speed = speed;
+        this.armored = armored;
+        this.weapon = weapon;
+    }
+
+    @Override
+    public Enemy clone(){
+        return new Enemy(type, health, speed, armored, weapon);
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void printStats() {
+        System.out.println(type + " [Health: " + health +
+                           ", Speed: " + speed +
+                           ", Armored: " + armored +
+                           ", Weapon: " + weapon + "]");
+    }
+}
+
+public class prototype{
+    public static void main(String[] args) {
+        Enemy e1 = new Enemy("Flying", 100, 100, true, "axe");
+        Enemy e2 = e1.clone();
+        Enemy e3 = new Enemy("Flying", 100, 100, true, "axe");
+    }
+}
